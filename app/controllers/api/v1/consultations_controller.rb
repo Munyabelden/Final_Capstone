e5acfcf72de66ab732e5bf95a238d98e636a4702
Gracerue <munyaradzi045@gmail.com>
2023-07-27 11:03:42 +0200

    Finish cosultation booking form functinality

--- app/controllers/api/v1/consultations_controller.rb
+++ app/controllers/api/v1/consultations_controller.rb
@@ -1,31 +1,28 @@
-class Api::V1::ConsultationsController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
-  before_action :set_consultation, only: [:show, :update, :destroy]
+class Api::V1::ConsultationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_consultation, only: %i[show update destroy]
 
-  # GET /consultations
   def index
     @consultations = Consultation.all
 
     render json: @consultations
   end
 
-  # GET /consultations/1
   def show
     render json: @consultation
   end
 
-  # POST /consultations
   def create
     @consultation = Consultation.new(consultation_params)
 
     if @consultation.save
       render json: @consultation, status: :created, location: @consultation
     else
+      puts @consultation.errors.full_messages
       render json: @consultation.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /consultations/1
   def update
     if @consultation.update(consultation_params)
       render json: @consultation
@@ -34,19 +31,17 @@ class Api::V1::ConsultationsController < ActionController::Base
     end
   end
 
-  # DELETE /consultations/1
   def destroy
     @consultation.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_consultation
-      @consultation = Consultation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def consultation_params
-      params.require(:consultation).permit(:user_id, :doctor_id, :duration, :city, :date, :type)
-    end
+  def set_consultation
+    @consultation = Consultation.find(params[:id])
+  end
+
+  def consultation_params
+    params.require(:consultation).permit(:user_id, :doctor_id, :duration, :city, :date, :consultation_type)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-    has_many :consultations
-  
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :phone, allow_blank: true, format: { with: /\A\d+\z/, message: 'should only contain digits' }
-  end
-  
+  has_many :consultations
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, allow_blank: true, format: { with: /\A\d+\z/, message: 'should only contain digits' }
+end
