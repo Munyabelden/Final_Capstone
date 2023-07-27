bbc1d19e9389fba37ef65948b6ac8780dab53f42
Belden Mugauri <munyaradzi045@gmail.com>
2023-07-27 10:26:36 +0200

    Fix syntax error

--- db/migrate/20230721115616_create_users.rb
+++ db/migrate/20230721115616_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :fisrt_name, null: false, default: ''
+      t.string :first_name, null: false, default: ''
       t.string :last_name, null: false, default: ''
       t.string :phone
 
