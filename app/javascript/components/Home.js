c10725a8dbe44cec3f2e2f6ce135264473fc4cff
Kelvin <ngotho.kelvin@yahoo.com>
2023-07-31 10:58:38 +0300

    Add display carousel

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -20,3 +20,40 @@ const Home = () => {
 
   const isMobile = window.innerWidth <= 768;
 
+  return (
+    <div className="home col-md col">
+      <h1 className="bold-font homepage-heading">
+        OUR DOCTORS
+      </h1>
+      <p className="gray-font">
+        Please select a doctor for an appointment
+      </p>
+      <hr className="dash home-dash" />
+      <div className="carousel-container col-12">
+        <Swiper
+          className="doctor-list col-10"
+          modules={[Navigation, A11y]}
+          spaceBetween={0}
+          slidesPerView={isMobile ? 1 : 3}
+          navigation
+        >
+          {doctors.map((doctor) => (
+            <SwiperSlide key={doctor.id}>
+              <DoctorCard
+                name={doctor.name}
+                specialization={doctor.specialization}
+                bio={doctor.bio}
+                image={doctor.image}
+                experience={doctor.experience}
+                rate={doctor.rate}
+                id={doctor.id}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
