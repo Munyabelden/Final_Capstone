6d4a0bcfc90d70d312fd755252af5e7cee2df6d4
BB-Simon <simon.chowdery@gmail.com>
2023-07-31 14:54:32 +0300

    Add user session base controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root "root#index"
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'api/v1/login',
+    sign_out: 'api/v1/logout',
+    registration: 'api/v1/signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   namespace :api do
     namespace :v1 do
