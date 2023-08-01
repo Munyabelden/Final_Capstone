c1a201df3cdee834e1a706f198ce160e63bf81f4
Abe1able <abel.workmail@gmail.com>
2023-08-01 13:18:48 +0300

    Finalize doctor details page

--- app/javascript/components/DoctorDetails.js
+++ app/javascript/components/DoctorDetails.js
@@ -2,75 +2,32 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const DoctorDetails = () => {
-  // const { id } = useParams();
-  // console.log(id);
-  // const [Doctor, setDoctor] = useState({});
-  // const params = useParams();
 
-  // useEffect(() => {
-  //   const getDoctor = async () => {
-  //     const result = await fetch(`http://localhost:3030/Doctors/${params.id}`);
-  //     const data = await result.json();
-
-  //     setDoctor(data);
-  //   };
-  //   getDoctor();
-  // }, []);
-
-  const { id } = useParams(); 
-  //console.log(id);
-  const [doctor, setDoctor] = useState(null);
+  const [doctor, setDoctor] = useState({});
+  const params = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:3030/doctors`)
-      .then(response => response.json())
-      .then(data => setDoctor(data));
-  }, [id]);
-  console.log(doctor);
-  // const {  image, name, experience, rate, specialization, bio } = doctor;
+    const getDoctor = async () => {
+      const result = await fetch(`http://localhost:3030/doctors/1`);
+      const data = await result.json();
+      console.log(data);
+      setDoctor(data);
+    };
+    getDoctor();
+  }, []);
+
+  const { id, image, name, experience, rate, specialization, bio } = doctor;
   return (
-    // <section>
-    //   <div>
-    //     <div>
-    //       {/* <img src={image} alt={name} /> */}
-    //     </div>
-
-    //     <div>
-    //       <h2>
-    //         {/* {name} */}
-    //         {doctor.name}
-    //       </h2>
-
-    //       <p>
-    //         {/* {bio} */}
-    //       </p>
-    //       <span>
-    //         {/* {experience} */}
-    //       </span>
-    //       <span>
-    //         {/* {rate} */}
-    //       </span>
-
-    //       <div>
-    //         <div>
-    //           <a href="#">
-    //             <span>Book Consultation</span>
-    //           </a>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
     <div>
     {doctor ? (
       <section>
-        <img src={doctor[0].image} />
+        <img src={image} />
         <div>
-        <h2>NAME: {doctor[0].name}</h2>
-        <p>PRICE: {doctor[0].rate}$</p>
-        <p>BIO: {doctor[0].bio}</p>
-        <p>SPECIALIZATION: {doctor[0].specialization}</p>
-        <p>EXPERIENCE: {doctor[0].experience}</p>
+        <h2>NAME: {name}</h2>
+        <p>PRICE: {rate}$</p>
+        <p>BIO: {bio}</p>
+        <p>SPECIALIZATION: {specialization}</p>
+        <p>EXPERIENCE: {experience}</p>
         <button>Book Consultation</button>
         </div>
       </section>
