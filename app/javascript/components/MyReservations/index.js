70f3110f789ed32b84d78777ead07fdefe8b0179
BB-Simon <simon.chowdery@gmail.com>
2023-08-02 19:40:43 +0300

    Add my reservation

--- app/javascript/components/MyReservations/index.js
+++ app/javascript/components/MyReservations/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
 const MyReservations = ({}) => {
-  return <div>MyReservations</div>
+  const consultations = [
+    {
+      name: "Meet with D. A.K. Hasan",
+      type: 'Online',
+      location: 'Online',
+      date: '2023-01-23',
+    },
+    {
+      name: "Meet with D. A.K. Hasan",
+      type: 'Online',
+      location: 'Online',
+      date: '2023-01-23',
+    },
+    {
+      name: "Meet with D. A.K. Hasan",
+      type: 'Inplace',
+      location: 'City hall, down town, New York',
+      date: '2023-01-23',
+    },
+    {
+      name: "Meet with D. A.K. Hasan",
+      type: 'Inplace',
+      location: 'City hall, down town, New York',
+      date: '2023-01-23',
+    },
+  ]
+  return (
+    <div className='consultation-container'>
+      <ul className='list-container'>
+        {consultations.map((c, index) => (
+          <li className='list-item' key={index}>
+            <h3 className='consultation-title'>{c.name}</h3>
+            <span className='consultation-type'>
+              <i class="fa-solid fa-house-medical"></i>
+              {c.type}
+            </span>
+            <span className='consultation-date'>
+              <i class="fa-solid fa-calendar-days"></i>
+              {c.date}
+            </span>
+            <p className='consultation-location'>
+              <i class="fa-solid fa-location-dot"></i>
+              {c.location}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default MyReservations;
